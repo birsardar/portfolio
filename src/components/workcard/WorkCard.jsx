68d4120c90dc091aaeb4da4fbe6c7e6f800483d4
birsardar <100059762+birsardar@users.noreply.github.com>
2023-06-22 14:31:23 +0545

    some changes

--- src/components/workcard/WorkCard.jsx
+++ src/components/workcard/WorkCard.jsx
@@ -1,31 +1,60 @@
 import { NavLink } from "react-router-dom";
 import "./workcard.css";
-import React from "react";
+import React, { useRef, useState } from "react";
 
 export default function WorkCard(props) {
+  const contentRef = useRef(null);
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [expandedIndex, setExpandedIndex] = useState(null);
+
+  const toggleContent = (event) => {
+    if (expandedIndex === props.text) {
+      setExpandedIndex(null);
+    } else {
+      setExpandedIndex(props.text);
+    }
+  };
+  const handleClick = (event) => {
+    event.preventDefault();
+    // Perform form submission logic here
+
+    // Reset the form fields
+    event.target.reset();
+
+    // Set the isSubmitted state to true
+  };
   return (
-    <div className="project-card">
-      <img src={props.imgsrc} alt="imageloading" />
-      <h2 className="project-title">
-        {props.title.length > 50
-          ? props.title.slice(0, 40) + "..."
-          : props.title}
-      </h2>
-      <div className="project-details">
-        <p>
-          {props.text.length > 100
-            ? props.text.slice(0, 100) + "..."
-            : props.text}
-        </p>
-        <div className="pro-btns">
-          <NavLink to={props.view} className="btn">
-            View
-          </NavLink>
-          <NavLink to="url.com" className="btn">
-            Souce
-          </NavLink>
+    <>
+      <div className="project-card" ref={contentRef}>
+        <img src={props.imgsrc} alt="imageloading" />
+        <h2 className="project-title">
+          {props.title.length > 50
+            ? props.title.slice(0, 40) + "..."
+            : props.title}
+        </h2>
+        <div className="project-details">
+          <p>
+            {props.text.length > 100
+              ? props.text.slice(0, 100) + "..."
+              : props.text}
+          </p>
+          <div className="pro-btns">
+            <NavLink to={props.view} className="btn">
+              View
+            </NavLink>
+            <button onClick={toggleContent} className="btn">
+              {expandedIndex ? "Collapse" : "Expand"}
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+      {expandedIndex && (
+        <div onClick={handleClick}>
+          <div className="expanded-content">
+            {props.text}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
