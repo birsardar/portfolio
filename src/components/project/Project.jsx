d43f92fba13f3315fb57f6defa072645b2e1aa6f
birsardar <100059762+birsardar@users.noreply.github.com>
2023-07-08 11:07:43 +0545

    fixes

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../navbar/Navbar";
 import Footer from "../footer/Footer";
 import Heroimage2 from "../heroimage/Heroimage2";
-import Card from "../card/Card";
+// import Card from "../card/Card";
 import Work from "../workcard/Work";
 
 export default function project() {
